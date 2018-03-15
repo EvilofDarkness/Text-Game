381e60032867d8c36f343807f082ae827e062c2f
EvilofDarkness <37348290+EvilofDarkness@users.noreply.github.com>
2018-03-15 17:30:21 +0100

    1.6 Alpha Version of Game
    1.5 Version Changes:
    - Damage Draw Added,
    - Saves Added,
    - Save Menu Added,
    - Now Game Load Last Save On Start

--- game.cpp
+++ game.cpp
@@ -7,29 +7,39 @@
 using namespace std;
 //hp is player healht points, dmg is player damage
 //hp_e is enemy health, dmg_e is enemy damege
-int hp=1000, dmg, hp_e, dmg_e;
+int hp=1000, dmg_ch, dmg, hp_e, dmg_e, dmg_e_max;
 string enemy, nickname;
+void walka();
+void get_enemy();
+void select_name();
+void main_menu();
+void save_read();
+//Test Dmg Script
+void damage()
+{
+	srand(time(NULL));
+	dmg_e=rand()%((dmg_e_max/2)+(dmg_e_max/4))+(dmg_e_max/4);
+	dmg=rand()%((dmg_ch/2)+(dmg_ch/4))+(dmg_ch/4);
+}
 //Fight Script
 void walka()
 {
-	void get_enemy();
-	void main_menu();
 	get_enemy();
 	for(hp, hp_e;hp>=0 or hp_e>=0;hp-=dmg_e, hp_e-=dmg)
 	{
 		system("cls");
+		damage();
 		cout<<nickname<<": "<<hp<<" | "<<enemy<<": "<<hp_e<<endl;
 		cout<<"--------------------------"<<endl;
-		cout<<enemy<<" gets "<<dmg<<" dmg"<<endl;
-		cout<<nickname<<" gets "<<dmg_e<<" dmg"<<endl;
+		cout<<enemy<<" deal "<<dmg_e<<" dmg"<<endl;
+		cout<<nickname<<" deal "<<dmg<<" dmg"<<endl;
+		Sleep(1000);
 		if(hp<=0)
 		{
 			system("cls");
 			cout<<"--------------------------"<<endl;
 			cout<<"Your fight with "<<enemy<<" has ended Defeat!"<<endl;
 			cout<<"--------------------------"<<endl;
-			getch();
-			main_menu();
 			break;
 		}
 		if(hp_e<=0)
@@ -38,12 +48,12 @@ void walka()
 			cout<<"--------------------------"<<endl;
 			cout<<"Your fight with "<<enemy<<" has ended Victory!"<<endl;
 			cout<<"--------------------------"<<endl;
-			getch();
-			main_menu();
 			break;
 		}
-		Sleep(600);
 	}
+	Sleep(1000);
+	getch();
+	main_menu();
 }
 //Generate Enemy Script
 void get_enemy()
@@ -53,26 +63,26 @@ void get_enemy()
 	switch(villain)
 	{
 		case 1:
-			{
-				hp_e=230;
-				dmg_e=30;
-				enemy="Wolf";
-				break;
-			}
+		{
+			hp_e=230;
+			dmg_e_max=30;
+			enemy="Wolf";
+			break;
+		}
 		case 2:
-			{
-				hp_e=120;
-				dmg_e=15;
-				enemy="Rabbit";
-				break;
-			}
+		{
+			hp_e=120;
+			dmg_e_max=15;
+			enemy="Rabbit";
+			break;
+		}
 		case 3:
-			{
-				hp_e=600;
-				dmg_e=55;
-				enemy="Mountain Bear";
-				break;
-			}
+		{
+			hp_e=600;
+			dmg_e_max=55;
+			enemy="Mountain Bear";
+			break;
+		}
 	}
 }
 //Name Select Script
@@ -83,6 +93,8 @@ void select_name()
 	cout<<"Enter Your Name."<<endl;
 	cout<<"--------------------------"<<endl;
 	getline(cin, nickname);
+	Sleep(200);
+	main_menu();
 }
 //Main Menu Script
 void main_menu()
@@ -93,36 +105,132 @@ void main_menu()
 	cout<<"Welcome "<<nickname<<endl;
 	cout<<"What you want to do?"<<endl;
 	cout<<"(1) Fight"<<endl;
-	cout<<"(2) Exit"<<endl;
+	cout<<"(2) Save"<<endl;
+	cout<<"(3) Exit"<<endl;
 	cout<<"--------------------------"<<endl;
 	cout<<nickname<<": ";
 	choice=getch();
 	cout<<endl;
-	Sleep(700);
+	Sleep(600);
 	switch(choice)
 	{
 		case '1':
-			{
-				walka();
-				break;
-			}
+		{
+			walka();
+			break;
+		}
 		case '2':
-			{
-				system("pause");
-				break;
-			}
+		{
+			save_read();
+			break;
+		}			
+		case '3':
+		{
+			system("pause");
+			break;
+		}
 		default:
+		{
+			main_menu();
+		}
+	}
+}
+//Save and Load Game State Script
+void save_read()
+{
+	int choice;
+	system("cls");
+	cout<<"--------------------------"<<endl;
+	cout<<"Do you want save/load game state?"<<endl;
+	cout<<"(1) Save"<<endl;
+	cout<<"(2) Load"<<endl;
+	cout<<"(3) Back"<<endl;
+	cout<<"--------------------------"<<endl;
+	cout<<nickname<<": ";
+	choice=getch();
+	Sleep(600);
+	switch(choice)
+	{
+		case '1':
+		{
+			fstream save("saves/save.txt", ios::out);
+			if(save.good())
 			{
-				main_menu();
+				system("cls");
+				save<<nickname<<endl;
+				save<<hp<<endl;
+				save<<dmg_ch<<endl;
+				cout<<"--------------------------"<<endl;
+				cout<<"Game state Saved!"<<endl;
+				cout<<"--------------------------"<<endl;
+				save.close();
 			}
+			else
+			{
+				system("cls");
+				system("md saves");
+				cout<<"--------------------------"<<endl;
+				cout<<"Cannot save game state!"<<endl;
+				cout<<"--------------------------"<<endl;
+				save.close();
+			}
+			break;
+		}
+		case '2':
+		{
+			system("cls");
+			fstream read("saves/save.txt", ios::in);
+			if(read.good())
+			{
+				system("cls");
+				read>>nickname>>hp>>dmg_ch;
+				cout<<"--------------------------"<<endl;
+				cout<<"Game state loaded!"<<endl;
+				cout<<"--------------------------"<<endl;
+				read.close();
+			}
+			else
+			{
+				system("cls");
+				cout<<"--------------------------"<<endl;
+				cout<<"Cannot load game state!"<<endl;
+				cout<<"--------------------------"<<endl;
+				read.close();
+			}
+			break;
+		}
+		case '3':
+		{
+			main_menu();
+			break;
+		}
+		default:
+		{
+			save_read();
+		}
+	}
+	Sleep(1200);
+	save_read();
+}
+//Game start save load
+void start()
+{
+	fstream start("saves/save.txt", ios::in);
+	if(start.good())
+	{
+		start>>nickname>>hp>>dmg_ch;
+		main_menu();
+	}
+	else
+	{
+		select_name();
 	}
 }
 //Main Function
 int main()
 {
 	setlocale(LC_ALL, "");
-	select_name();
-	dmg=25;
-	main_menu();
+	dmg_ch=25;
+	start();
 	return 0;
 }
